e27f246da1da4c7d858b580321d60d2e083b73ff
GeekBar-Boss <920435257@qq.com>
2021-05-23 22:50:31 +0800

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -126,10 +126,7 @@ function highlightAllBlock(){
 function setUpleftcornerButton(PromptFlag){
     if(PromptFlag){
 	for(let i=0;i<6;i++){
-		$(".menu-wrapper").fadeToggle(1000);
-		if($(".menu-wrapper").css("display")=="none"){
-			$(".menu-wrapper").fadeToggle(1000);
-		}
+		$(".menu-wrapper").deley(1000).fadeToggle(1000.function(){$(".menu-wrapper").fadeToggle(1000);});
 	}
     }
     $(".menu-wrapper").fadeTo("fast",0.01);
